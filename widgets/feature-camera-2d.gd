9250074bf54060058aec0d7c21a1d50a42e162db
Dave Kerr <dave@naturallyintelligent.com>
2023-02-28 12:28:49 -0700

    Update FeatureCamera2D to v4

--- widgets/feature-camera-2d.gd
+++ widgets/feature-camera-2d.gd
@@ -1,3 +1,4 @@
+# FEATURE CAMERA 2D
 extends Camera2D
 class_name FeatureCamera2D
 
@@ -12,24 +13,28 @@ var target: Node2D : set = set_target
 @export var target_behind_pixels := 0
 @export var target_ahead_factor := 1
 @export var target_behind_factor := 1
-@export var target_ahead_vector := Vector2.ZERO
+@export var target_ahead_y := false
 var target_point := Vector2.ZERO
-var last_camera_direction_x := 0
-#var last_camera_direction := Vector2.ZERO
+var last_camera_direction := Vector2.ZERO
+
+# shake
+var shaking := false
 
 # earthquake
 @onready var noise := FastNoiseLite.new()
-var quake_decay := 0.8  # How quickly the shaking stops [0, 1].
+@export var max_shake_offset := Vector2(10, 5)  # Maximum hor/ver shake in pixels.
+@export var allow_shake_y := false
+@export var quake_decay := 0.8  # How quickly the shaking stops [0, 1].
+@export var quake_power := 2  # quake exponent. Use [2, 3].
 var quake := 0.0  # Current shake strength.
-var quake_power := 2  # quake exponent. Use [2, 3].
-var quake_noise := 0
-var max_shake_offset := Vector2(10, 5)  # Maximum hor/ver shake in pixels.
-var max_shake_roll := 0.1  # Maximum rotation in radians (use sparingly).
-
 # shake due to shooting
+@export var shoot_shake_decay := 2.0
+@export var shoot_shake_power := 2
 var shoot_shake := 0.0
-var shoot_shake_decay := 2.0
-var shoot_shake_power := 2
+var max_shake_roll := 0.1  # Maximum rotation in radians (use sparingly).
+var noise_vector := Vector2.ZERO
+var shake_vector := Vector2.ZERO
+
 
 # camera trigger areas - parent container of CameraTrigger objects
 @export var camera_trigger_areas: NodePath = NodePath("")
@@ -41,11 +46,14 @@ var last_trigger_area := ''
 var initial_camera_left_limit := 0
 var initial_camera_right_limit := 0
 
+const TARGET_CATCHUP_LERP_SPEED = 0.05
+const NOISE_FACTOR = 1
+
 func _ready():
 	# random noise
-	noise.seed = randi()
-	#noise.period = 4
-	#noise.octaves = 2
+	noise.seed = 5
+	noise.frequency = 0.88
+	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
 	# timers
 	$DramaticTimer.connect("timeout",Callable(self,"after_drama"))
 	# limits
@@ -63,56 +71,23 @@ func _process(delta):
 	if not target_ref or not target_ref.get_ref():
 		target_ref = null
 		target = null
-	# trigger areas
-	# todo: optimize with Area2D
-	if trigger_areas and target:
-		for trigger_area in trigger_areas.get_children():
-			if trigger_area.check_trigger(target.global_position):
-				if trigger_area.name != last_trigger_area:
-					#debug.print('Inside', trigger_area.name)
-					# tween
-					var tween: Tween = create_tween()
-					var time = 1.5
-					var trans_type = Tween.TRANS_CUBIC
-					var ease_type = Tween.EASE_OUT
-					tween.stop_all()
-					tween.remove_all()
-					# y limits
-					if trigger_area.new_y_limits:
-						tween.tween_property(self, "limit_top", trigger_area.limit_y_top, time)
-						tween.set_trans(trans_type)
-						tween.set_ease(ease_type)
-						tween.tween_property(self, "limit_bottom", trigger_area.limit_y_bottom, time)
-						tween.set_trans(trans_type)
-						tween.set_trans(ease_type)
-					# x limits
-					if trigger_area.new_x_limits:
-						limit_left = trigger_area.limit_x_left
-						limit_right = trigger_area.limit_x_right
-						initial_camera_left_limit = limit_left
-						initial_camera_right_limit = limit_right
-					# target distance pixels
-					if trigger_area.new_target_ahead:
-						target_ahead_pixels = trigger_area.target_ahead_pixels
-						target_behind_pixels = trigger_area.target_behind_pixels
-					last_trigger_area = trigger_area.name
-					call_deferred("check_empty_triggers")
-
-# MOVEMENT
-func _physics_process(delta):
+	# target follow
 	if target:
 		if target_ahead:
 			target_ahead_camera(delta)
 		else:
 			global_position = target.global_position
-	if quake:
-		if quake > 0:
-			quake = max(quake - quake_decay * delta, 0)
-			do_shake(quake, quake_power)
-	if shoot_shake:
-		if shoot_shake > 0:
-			shoot_shake = max(shoot_shake - shoot_shake_decay * delta, 0)
-			do_shake(shoot_shake, shoot_shake_power)
+	# trigger areas
+	if trigger_areas and target:
+		for trigger_area in trigger_areas.get_children():
+			# todo: optimize with Area2D?
+			if trigger_area.check_trigger(target.global_position):
+				if trigger_area.name != last_trigger_area:
+					enter_trigger_area(trigger_area)
+	# shake
+	if shaking:
+		_shake_camera(delta)
+	# remember last position
 	last_camera_position = global_position
 
 # TARGET
@@ -121,61 +96,130 @@ func set_target(_target):
 	target = _target
 
 func target_ahead_camera(delta):
-	var new_direction_x = sign(target.direction.x)
-	if new_direction_x == 0:
-		new_direction_x = last_camera_direction_x
-
-	if new_direction_x != last_camera_direction_x:
+	# find direction based on target
+	var new_direction: Vector2
+	new_direction.x = sign(target.direction.x)
+	new_direction.y = sign(target.direction.y)
+	if new_direction.x == 0:
+		new_direction.x = last_camera_direction.x
+	if new_direction.y == 0:
+		new_direction.y = last_camera_direction.y
+	# switch direction?
+	if new_direction.x != last_camera_direction.x \
+	or (target_ahead_y and new_direction.y != last_camera_direction.y):
 		$TargetAheadTimer.start()
-
+	# move target point?
 	if $TargetAheadTimer.is_stopped():
 		if abs(target.velocity.x) > 0:
-			if new_direction_x > 0:
+			if new_direction.x > 0:
 				target_point.x += target_ahead_factor
 				if target_point.x > target_ahead_pixels:
 					target_point.x = target_ahead_pixels
-			elif new_direction_x < 0:
+			elif new_direction.x < 0:
 				target_point.x -= target_behind_factor
 				if target_point.x < -target_behind_pixels:
 					target_point.x = -target_behind_pixels
+	# move towards goal
+	global_position = lerp(global_position, target.global_position + target_point, TARGET_CATCHUP_LERP_SPEED)
+	# remember direction
+	last_camera_direction = new_direction
 
+# TRIGGER
+func enter_trigger_area(trigger_area: CameraTrigger):
+	#debug.print('Inside', trigger_area.name)
+	# tween
 	var tween: Tween = create_tween()
-	tween.tween_property(self, "global_position", target.global_position + target_point, 0.2)
-	tween.set_trans(Tween.TRANS_SINE)
-	tween.set_ease(Tween.EASE_OUT)
-
-	last_camera_direction_x = new_direction_x
+	var time = 1.5
+	var trans_type = Tween.TRANS_CUBIC
+	var ease_type = Tween.EASE_OUT
+	# y limits
+	if trigger_area.new_y_limits:
+		tween.set_trans(trans_type)
+		tween.set_ease(ease_type)
+		tween.tween_property(self, "limit_top", trigger_area.limit_y_top, time)
+		tween.tween_property(self, "limit_bottom", trigger_area.limit_y_bottom, time)
+	# x limits
+	if trigger_area.new_x_limits:
+		limit_left = trigger_area.limit_x_left
+		limit_right = trigger_area.limit_x_right
+		initial_camera_left_limit = limit_left
+		initial_camera_right_limit = limit_right
+	# target distance pixels
+	if trigger_area.new_target_ahead:
+		target_ahead_pixels = trigger_area.target_ahead_pixels
+		target_behind_pixels = trigger_area.target_behind_pixels
+	last_trigger_area = trigger_area.name
+	call_deferred("check_empty_triggers")
 
 # SHAKE / QUAKE
-func do_shake(_shake, _shake_power):
-	var amount = pow(_shake, _shake_power)
-	quake_noise += 1
-	rotation = max_shake_roll * amount * noise.get_noise_2d(noise.seed, quake_noise)
-	offset.x = max_shake_offset.x * amount * noise.get_noise_2d(noise.seed*2, quake_noise)
-	offset.y = max_shake_offset.y * amount * noise.get_noise_2d(noise.seed*3, quake_noise)
-	if offset.y > 0:
-		offset.y = 0
+func start_shaking():
+	shaking = true
+	
+func stop_shaking():
+	shaking = false
+	shake_vector = Vector2.ZERO
+	offset = Vector2.ZERO
 
-func shake(amount):
-	return
+func _shake_camera(delta):
+	# quake
+	var continue_shaking = false
+	if quake:
+		if quake > 0:
+			quake = max(quake - quake_decay * delta, 0)
+			_do_shake(quake, quake_power)
+			if quake > 0:
+				continue_shaking = true
+	# shoot shake
+	if shoot_shake > 0:
+		shoot_shake = max(shoot_shake - shoot_shake_decay * delta, 0)
+		_do_shake(shoot_shake, shoot_shake_power)
+		if shoot_shake > 0:
+			continue_shaking = true
+	if not continue_shaking:
+		stop_shaking()
+
+func _do_shake(_shake, _shake_power):
+	# get the noise
+	var shake_offset = Vector2.ZERO
+	var shake_amount = pow(_shake, _shake_power)
+	noise_vector.x += 1
+	noise_vector.y += 1
+	shake_offset.x = max_shake_offset.x * shake_amount * noise.get_noise_2d(noise.seed*2, noise_vector.x) * NOISE_FACTOR
+	if allow_shake_y:
+		shake_offset.y = max_shake_offset.y * shake_amount * noise.get_noise_2d(noise.seed*3, noise_vector.y) * NOISE_FACTOR
+	# apply offset
+	shake_vector = shake_offset
+	# clamp to max shake
+	shake_vector.x = clamp(shake_vector.x, -max_shake_offset.x, max_shake_offset.x)
+	shake_vector.y = clamp(shake_vector.y, -max_shake_offset.y, max_shake_offset.y)
+	#if not shake_y: shake_vector.y = 0
+	shake_vector.x = int(shake_vector.x)
+	shake_vector.y = int(shake_vector.y)
+	offset = shake_vector
+	# rotation
+	#rotation = shake_amount * noise.get_noise_2d(noise.seed, noise_vector.x)
+	#rotation = clamp(rotation, -max_shake_roll, max_shake_roll)
+
+func quake_shake(amount):
 	quake = min(quake + amount, 1.0)
+	start_shaking()
 
 func shooty_shake(_direction):
-	return
 	shoot_shake = 1
+	start_shaking()
 
 # DRAMA
 func zoom_drama(wait=0.25, zoom_factor=0.5):
-	zoom_to(zoom_factor)
+	zoom_to(1.0+zoom_factor)
 	$DramaticTimer.wait_time = wait
 	$DramaticTimer.start()
 
 func after_drama():
 	#zoom_normal()
 	var tween: Tween = create_tween()
-	tween.tween_property(self, "zoom", Vector2(1.0,1.0), 0.22)
 	tween.set_trans(Tween.TRANS_SINE)
 	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(self, "zoom", Vector2(1.0,1.0), 0.22)
 
 # ZOOM
 func zoom_normal():
@@ -187,28 +231,27 @@ func zoom_to(_target):
 	zoom.y = _target
 
 func zoom_in(amount):
-	var min_zoom = 0.5
-	zoom.x -= amount
-	zoom.y -= amount
-	if zoom.x < min_zoom:
+	var min_zoom = 4.00
+	zoom.x += amount
+	zoom.y += amount
+	if zoom.x > min_zoom:
 		zoom.x = min_zoom
-	if zoom.y < min_zoom:
+	if zoom.y > min_zoom:
 		zoom.y = min_zoom
 	if target:
 		global_position = target.global_position
 
 func zoom_out(amount):
-	var max_zoom = 1.5
-	zoom.x += amount
-	zoom.y += amount
-	if zoom.x > max_zoom:
+	var max_zoom = 0.5
+	zoom.x -= amount
+	zoom.y -= amount
+	if zoom.x < max_zoom:
 		zoom.x = max_zoom
-	if zoom.y > max_zoom:
+	if zoom.y < max_zoom:
 		zoom.y = max_zoom
 	if zoom.y > 1.0:
-		limit_top = int(-640.0*(zoom.y-1.0))
-	else:
-		limit_top = 0
+		limit_top = -10000
+		limit_bottom = 10000
 	if target:
 		global_position = target.global_position
 
@@ -245,7 +288,7 @@ func set_smoothing_speed_temporarily(speed=1, time=2.5):
 	var old_speed = position_smoothing_speed
 	position_smoothing_speed = speed
 	var tween: Tween = create_tween()
-	tween.tween_property(self, 'position_smoothing_speed', old_speed, time)
 	tween.set_trans(Tween.TRANS_CUBIC)
 	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(self, 'position_smoothing_speed', old_speed, time)
 	await tween.finished
