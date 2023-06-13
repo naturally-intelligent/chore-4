554ef822e6d739541d26468c91e2e073ea55ce36
Dave Kerr <dave@naturallyintelligent.com>
2023-06-12 20:10:15 -0600

    Script GIF create crop notes.

--- engine/lib/create-gif.sh
+++ engine/lib/create-gif.sh
@@ -1,4 +1,8 @@
-gifsicle -O3 gifimage1.gif -o new-gifimage1.gif
+# BASIC
 convert -delay 10 -loop 0 *.png anim.gif
+# CROP
+convert -delay 5 -loop 0 -coalesce -repage 0x0 -crop 464x260+86+26 +repage *.png anim.gif
+# COMPRESS
+gifsicle -O3 gifimage1.gif -o new-gifimage1.gif
+# SCALE
 gifsicle --delay=5 -O3 --scale=0.5 anim.gif -o final.gif
-
