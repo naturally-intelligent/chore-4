c3bc6f121208d999cf2e5d37c0320315afcb6f2f
Dave Kerr <dave@naturallyintelligent.com>
2023-06-15 11:26:37 -0600

    GIF commands

--- engine/lib/create-gif.sh
+++ engine/lib/create-gif.sh
@@ -6,3 +6,6 @@ convert -delay 5 -loop 0 -coalesce -repage 0x0 -crop 464x260+86+26 +repage *.png
 gifsicle -O3 gifimage1.gif -o new-gifimage1.gif
 # SCALE
 gifsicle --delay=5 -O3 --scale=0.5 anim.gif -o final.gif
+
+# FROM VIDEO
+ffmpeg -i devday-battle1.mkv -vf "fps=10,scale=320:-1:flags=lanczos" -c:v pam -f image2pipe - | convert -delay 10 - -loop 0 -layers optimize output.gif
